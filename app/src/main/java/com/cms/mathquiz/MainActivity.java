cccca7752cf86153cc13b2a28fe37e6f717d45d6
FZhu <hzhu225@gmail.com>
2019-01-11 11:51:59 +1100

    Finish question 4 and change submit button

--- app/src/main/java/com/cms/mathquiz/MainActivity.java
+++ app/src/main/java/com/cms/mathquiz/MainActivity.java
@@ -11,8 +11,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
-    String message;
-    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,70 +19,49 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
     }
 
-    public void checkQ1(View view)
-    {
-        RadioButton rb1 = findViewById(R.id.q1RB1);
-        RadioButton rb2 = findViewById(R.id.q1RB2);
-        RadioButton rb3 = findViewById(R.id.q1RB3);
 
-        if(rb3.isChecked())              //radioButton3 is the correct answer
-        {
-            message = "Congratulations! 9.5 is the correct answer.";
-        }
-        else if (rb1.isChecked() || rb2.isChecked())           //radioButton1 or radioButton2 is checked
-        {
-            message = "Sorry, your answer is incorrect.";
-        }
-        else                                                  //no radio button is checked
-        {
-            message = "Please answer the question first.";
-        }
-        toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.TOP,0,10);
-        toast.show();
-    }
-
-    public void checkQ2(View view)
+    public void submit_Click(View view)
     {
+        int totalMark = 0;
+
+        //Check question 1
+        RadioButton q1RB3 = findViewById(R.id.q1RadioButton3);
+        if(q1RB3.isChecked())
+        {
+            totalMark += 25;
+        }
+
+        //Check question 2
         EditText q2Result = findViewById(R.id.q2EditText);
         if(q2Result.getText().toString().equals("286"))
         {
-            message = "Congratulations! 286 is the correct answer.";
+            totalMark += 30;
         }
-        else if (q2Result.getText().toString().equals(""))
-        {
-            message = "Please answer the question first.";
-        }
-        else
-        {
-            message = "Sorry, your answer is incorrect.";
-        }
-        toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.TOP,0,10);
-        toast.show();
-    }
 
-
-    public void checkQ3(View view)
-    {
+        //Check question 3
         CheckBox q3CB1 = findViewById(R.id.q3CheckBox1);
         CheckBox q3CB2 = findViewById(R.id.q3CheckBox2);
         CheckBox q3CB3 = findViewById(R.id.q3CheckBox3);
         CheckBox q3CB4 = findViewById(R.id.q3CheckBox4);
         CheckBox q3CB5 = findViewById(R.id.q3CheckBox5);
-
         if(!q3CB1.isChecked() && q3CB2.isChecked() && q3CB3.isChecked()&& !q3CB4.isChecked()&& !q3CB5.isChecked())
         {
-            message = "Congratulations! 59 and 499 are the only 2 prime numbers among them.";
+            totalMark += 15;
         }
-        else
+
+        //Check question 4
+        EditText q4Result = findViewById(R.id.q4EditText);
+        if(q4Result.getText().toString().equals("38"))
         {
-            message = "Sorry, your answer is incorrect.";
+            totalMark += 30;
         }
-        toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+
+        String message = "You get " + totalMark + " marks out of 100.";
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
         toast.setGravity(Gravity.TOP,0,10);
         toast.show();
     }
+
 
 
 }
